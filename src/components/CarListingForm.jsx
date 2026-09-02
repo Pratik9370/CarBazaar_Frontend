0354979905af1f69b3bede0931977fa77498bbf5
Pratik Jadhav <pratikvj9370@gmail.com>
2026-09-02 17:57:47 +0530

    backend server relocated on ec2

--- src/components/CarListingForm.jsx
+++ src/components/CarListingForm.jsx
@@ -187,7 +187,7 @@ export default function CarListingForm() {
             };
 
             const response = await fetch(
-                "http://localhost:3000/api/car/predict",
+                "http://13.61.35.6/api/car/predict",
                 {
                     method: "POST",
                     headers: {
